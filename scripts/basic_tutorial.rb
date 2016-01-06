1e6a0ea0eabbebe50cf0da58e0030e2915a9b7c4
Cody Markelz <rjmarkelz@ucdavis.edu>
2016-01-05 22:41:07 -0800

    basic notes and chords

---
+++ scripts/basic_tutorial.rb
@@ -0,0 +1,34 @@
+# Playing with basic Tutorial
+play 23.234 # do not limit to whole notes
+sleep 0.5 # change the sleep time between notes
+play 30  # lower numbers, lower pitch left side of piano
+sleep 0.5
+play 40
+
+play :C # play notes
+sleep 0.5
+play :D
+sleep 0.5
+play :E
+play 30
+
+play 23.234
+sleep 0.5
+play 30
+sleep 0.5
+play 40
+
+play :C3 # play notes in different octave
+sleep 0.5
+play :D3
+sleep 0.5
+play :E4
+play 30
+play 23.234
+sleep 0.5
+play 30
+sleep 0.5
+play 40
+
+play :Fs3 # sharp notes
+play :Eb3 # or flat notes
