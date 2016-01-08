380114b31a6d531997a5131dd5f73b566480b257
Cody Markelz <rjmarkelz@ucdavis.edu>
2016-01-07 23:51:31 -0800

    Switching Synths

--- scripts/basic_tutorial.rb
+++ scripts/basic_tutorial.rb
@@ -32,3 +32,19 @@ play 40
 
 play :Fs3 # sharp notes
 play :Eb3 # or flat notes
+
+use_synth :saw
+play 38
+sleep 0.25
+play 50
+sleep 0.25
+play 62
+sleep 0.25
+
+use_synth :prophet
+play 38
+sleep 0.25
+play 50
+sleep 0.25
+play 62
+sleep 0.25
